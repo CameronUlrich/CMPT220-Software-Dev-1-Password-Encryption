054687570a073dfd1657f8d2b7c806653a8e5a86
Cameron Ulrich <Cameron.Ulrich1@marist.edu>
2017-01-22 17:55:20 -0500

    Fixed Problem 2.5
    Fixded it so user can actually type in gratuity rate

--- labs/1/Problem2_5.java
+++ labs/1/Problem2_5.java
@@ -6,13 +6,14 @@ public class Problem2_5
 	public static void main(String[] args) 
 	{
 		Scanner input = new Scanner(System.in);
-		double gratuity = 0.15;
+		System.out.println("Enter your tip percent for your meal: ");
+		double gratuity = input.nextDouble();
 		System.out.println("Enter your total for your meal: ");
 		double total = input.nextDouble();
 		
 		double tip = (gratuity * total);
 		double subTotal = (tip + total);
-		System.out.println("Your total for your meal plus tip is " + subTotal);
+		System.out.println("Your total for your meal plus tip is " + subTotal * .1);
 
 	}
 
