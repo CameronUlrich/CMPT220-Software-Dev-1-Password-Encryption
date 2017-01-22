aef0494aed09ceee5363c06750dd8740518783c4
Cameron Ulrich <Cameron.Ulrich1@marist.edu>
2017-01-22 17:50:50 -0500

    Problem 2.5
    Completed a working version of Problem 2.5

---
+++ labs/1/Problem2_5.java
@@ -0,0 +1,19 @@
+package driver_proj0;
+import java.util.Scanner;
+public class Problem2_5 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner input = new Scanner(System.in);
+		double gratuity = 0.15;
+		System.out.println("Enter your total for your meal: ");
+		double total = input.nextDouble();
+		
+		double tip = (gratuity * total);
+		double subTotal = (tip + total);
+		System.out.println("Your total for your meal plus tip is " + subTotal);
+
+	}
+
+}
