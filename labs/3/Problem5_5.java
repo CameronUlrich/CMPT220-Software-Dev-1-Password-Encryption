376aafe123ad2b608d5aeae0534d81f614cf5f3c
Juan Arias <juan.arias@marist.edu>
2017-02-09 20:40:23 -0500

    Feedback on Lab 3

--- labs/3/Problem5_5.java
+++ labs/3/Problem5_5.java
@@ -9,6 +9,8 @@ public class Problem5_5
         System.out.printf("%-12s%-8s\n", "Pounds", "Kilograms");
 
 
+	    // JA: The table of the left had increment of 2. The one on the right was until 515.
+
         for (int i = 1, j = 20; i < 200; i++, j += 5) 
         {
             System.out.printf("%-12d%8.1f", i, i * 2.2);
