753e95e1a6050ae5b49fa066459efedde6aa476f
Cameron Ulrich <Cameron.Ulrich1@marist.edu>
2017-04-04 16:26:10 -0400

    Problem 12.5

---
+++ labs/9/Problem12_5.java
@@ -0,0 +1,14 @@
+public class Problem12_5 {
+
+    public static void main(String[] args) {
+
+        Triangle t1;
+        try {
+            t1 = new Triangle(1, 1, 2000);
+        } catch (IllegalTriangleException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
